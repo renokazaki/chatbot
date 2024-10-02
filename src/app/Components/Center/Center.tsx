4d3740e54e83a6d88dbe5e23df1b6308336d3bee
renokazaki <ren84690@gmail.com>
2024-10-02 21:15:03 +0900

    レイアウト一旦完了

---
+++ src/app/Components/Center/Center.tsx
@@ -0,0 +1,39 @@
+export const Center = () => {
+  return (
+    <>
+      <div className="flex flex-col items-start ">
+        Center
+        <div className="p-4 mb-4 bg-blue-600 rounded-xl">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+      </div>
+      <div className="flex flex-col items-end">
+        <div className="p-4 mb-4 bg-blue-600">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>{" "}
+        <div className="p-4 mb-4 bg-blue-600">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>{" "}
+        <div className="p-4 mb-4 bg-blue-600">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>{" "}
+        <div className="p-4 mb-4 bg-blue-600">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>{" "}
+        <div className="p-4 mb-4 bg-blue-600">meaaaaaaaaaassage</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+        <div className="p-4 mb-4 bg-blue-600">message</div>
+      </div>
+      <form className="fixed flex justify-center bottom-0 bg-black p-4 w-5/6 ">
+        <input
+          className="w-5/6 h-12 py-4 border-double mr-12 border-4 border-indigo-600"
+          placeholder="値を入力"
+        />
+        <button className="w-8 bg-amber-400 rounded-3xl">↑</button>
+      </form>
+    </>
+  );
+};
---
+++ src/app/Components/SideBar/SideBar.tsx
@@ -0,0 +1,56 @@
+export const SideBar = () => {
+  return (
+    <div className="">
+      <div className="mb-12">
+        <form className="fixed flex justify-start w-1/5 top-0 bg-red-600  ">
+          <input
+            className="h-12 w-4/5 py-4 border-double  border-4 border-indigo-600"
+            placeholder="値を入力"
+          />
+          <button className="w-8 bg-amber-400 rounded-3xl">↑</button>
+        </form>
+      </div>
+
+      <div className="flex flex-col items-start ">
+        Center
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">
+          meaaaaaaaaaassage
+        </div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+      </div>
+      <div className="flex flex-col items-start ">
+        Center
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">
+          meaaaaaaaaaassage
+        </div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+      </div>
+      <div className="flex flex-col items-start ">
+        Center
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">
+          meaaaaaaaaaassage
+        </div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+      </div>
+      <div className="flex flex-col items-start ">
+        Center
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">
+          meaaaaaaaaaassage
+        </div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+      </div>
+      <div className="flex flex-col items-start ">
+        Center
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">
+          meaaaaaaaaaassage
+        </div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+        <div className="p-4 mb-4 bg-slate-500 rounded-xl">message</div>
+      </div>
+    </div>
+  );
+};
