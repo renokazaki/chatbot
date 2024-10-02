560e1b7ee2e1692327581f28d336c7acf508d51b
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-10-02 21:44:23 +0900

    レイアウトレスポンシブ対応修正

--- src/app/Components/Center/Center.tsx
+++ src/app/Components/Center/Center.tsx
@@ -27,7 +27,7 @@ export const Center = () => {
         <div className="p-4 mb-4 bg-blue-600">message</div>
         <div className="p-4 mb-4 bg-blue-600">message</div>
       </div>
-      <form className="fixed flex justify-center bottom-0 bg-black p-4 w-5/6 ">
+      <form className="fixed flex justify-center bottom-0 bg-black p-4 w-full lg:w-4/5 ">
         <input
           className="w-5/6 h-12 py-4 border-double mr-12 border-4 border-indigo-600"
           placeholder="値を入力"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { SideBar } from "./Components/SideBar/SideBar";
 export default function Home() {
   return (
     <div className="flex flex-row h-screen">
-      <div className="w-1/5 overflow-y-auto bg-red-300">
+      <div className="w-1/5 overflow-y-auto bg-slate-300 hidden lg:block ">
         <SideBar />
       </div>
-      <div className="w-4/5 overflow-y-auto">
+      <div className="w-full lg:w-4/5 overflow-y-auto ">
         <Center />
       </div>
     </div>
