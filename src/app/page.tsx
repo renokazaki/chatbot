2fad808b00c99a89195ef19f44f91c58b4ee2b7c
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-10-02 21:15:03 +0900

    レイアウト一旦完了

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { Center } from "./Components/Center/Center";
+import { SideBar } from "./Components/SideBar/SideBar";
+
 export default function Home() {
-  return <div className="text-sm">hello</div>;
+  return (
+    <div className="flex flex-row h-screen">
+      <div className="w-1/5 overflow-y-auto bg-red-300">
+        <SideBar />
+      </div>
+      <div className="w-4/5 overflow-y-auto">
+        <Center />
+      </div>
+    </div>
+  );
 }
