0e03665c7525355986bd9a6034e3bd9d1004dac7
renokazaki <ren84690@gmail.com>
2024-10-03 19:31:31 +0900

    supabaseのroom名、room選択時に格納されているメッセージの取得と表示完了

--- utils/supabasefunction.ts
+++ utils/supabasefunction.ts
@@ -1,10 +1,21 @@
 import {supabase} from "./supabase"
 
-//取得用
-export const getAllMsg = async () =>{
-    const msg = await supabase.from("chat").select("*").order("id", { ascending: true }); // idで昇順に並べる
-    return msg
+//room取得用
+export const getRoom = async () =>{
+    const room = await supabase.from("room").select("*").order("id", { ascending: true }); // idで昇順に並べる
+    return room
 }
+
+//roomの詳細取得用
+export const getRoomDescription = async (roomId : number) =>{
+    const roomDescription = await supabase
+    .from("roomDescription")
+    .select("*")
+    .eq("room_id", roomId)
+    return roomDescription
+}
+
+
 
 // //追加用
 // export const addTodo = async (title : string, priority : number) =>{
